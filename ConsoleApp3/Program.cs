c344f791b908fc6de3c49f8e4c67beed4293a4fd
Hu1lu <harin.nikita-sergeev@yandex.ru>
2020-09-19 09:52:53 +0300

    disk

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,12 +1,21 @@
 using System;
+using System.IO;
 
 namespace ConsoleApp3
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            
+            DriveInfo[] disks = DriveInfo.GetDrives();
+            foreach(DriveInfo disk in disks)
+            {
+                Console.WriteLine("Name -") ; Console.WriteLine(disk.Name);
+                Console.WriteLine("Fyle system -") ; Console.WriteLine(disk.DriveFormat);
+                Console.WriteLine("Type -") ; Console.WriteLine(disk.DriveType);
+                Console.WriteLine("Total size -") ; Console.WriteLine(disk.TotalSize);
+                Console.WriteLine("Total free space -") ; Console.WriteLine(disk.TotalFreeSpace);
+            }
         }
     }
 }
